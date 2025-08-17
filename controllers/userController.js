8382c67357a79a53ae0deba1da073cc35a25227c
farhathulla <farhathulla01@gmail.com>
2025-08-17 17:08:24 +0530

    cookie cross site

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,7 +54,7 @@ const loginUser = asyncHandler(async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     res.status(200).json({
